dd4a967645e719aaefcb838b6dc42cb9992ce670
Zayd EL IMRANI <elimrani.z@gmail.com>
2023-09-16 23:36:50 +0100

    Pushing the build folder

---
+++ build/release.d.ts
@@ -0,0 +1,9 @@
+/** Exported memory */
+export declare const memory: WebAssembly.Memory;
+/**
+ * assembly/index/add
+ * @param a `i32`
+ * @param b `i32`
+ * @returns `i32`
+ */
+export declare function add(a: number, b: number): number;
